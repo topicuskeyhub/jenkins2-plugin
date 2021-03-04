8c81e56e74ab0d99d088d332604348de183dfb4e
Artur Baars <arthur_baars@hotmail.com>
2021-03-03 16:05:08 -0800

    Added some tests

---
+++ src/test/java/nl/topicus/keyhub/jenkins/credentials/username_password/KeyHubUsernamePasswordCredentialsSnapshotTakerTest.java
@@ -0,0 +1,35 @@
+package nl.topicus.keyhub.jenkins.credentials.username_password;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+
+import hudson.util.Secret;
+import nl.topicus.keyhub.jenkins.credentials.SecretSupplier;
+import nl.topicus.keyhub.jenkins.model.ClientCredentials;
+import nl.topicus.keyhub.jenkins.vault.IKeyHubCommunicationService;
+import nl.topicus.keyhub.jenkins.vault.KeyHubCommunicationService;
+
+public class KeyHubUsernamePasswordCredentialsSnapshotTakerTest {
+
+    @Test
+    public void KeyHubUsernameSnapshotTest() {
+        // Arrange
+        KeyHubUsernamePasswordCredentialsSnapshotTaker snapshotTaker = new KeyHubUsernamePasswordCredentialsSnapshotTaker();
+        IKeyHubCommunicationService mockService = mock(KeyHubCommunicationService.class);
+        ClientCredentials testClientCredentials = new ClientCredentials("testId", Secret.fromString("testSecret"));
+        KeyHubUsernamePasswordCredentials testCredential = KeyHubUsernamePasswordCredentials.KeyHubCredentialsBuilder.newInstance()
+        .id("testId").recordName("testName")
+        .href("testHref").username("testUsername")
+        .password(new SecretSupplier(mockService, testClientCredentials, "testHref")).build();
+
+        // Act
+        KeyHubUsernamePasswordCredentials usernamePasswordCredential = snapshotTaker.snapshot(testCredential);
+
+        //Assert
+        assertEquals(testCredential, usernamePasswordCredential);
+
+    }
+    
+}
